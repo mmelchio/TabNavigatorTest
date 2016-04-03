b93203548fb6081bb5667bed98b4987500eaaa44
mmelchio <mmelchio@hotmail.ch>
2016-04-03 19:47:23 +0200

    testing

--- index.android.js
+++ index.android.js
@@ -2,49 +2,40 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
+
 import TabNavigator from 'react-native-tab-navigator';
 import React, {
   AppRegistry,
   Component,
   StyleSheet,
   Text,
-  View
+  View,
+  Image
 } from 'react-native';
 
-class TabNavigatorTest extends Component {
+class TabNavigatorTest extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Shake or press menu button for dev menu
-        </Text>
-        <TabNavigator>
-          <TabNavigator.Item
-            selected={this.state.selectedTab === 'home'}
-            title="Home"
-            renderIcon={() => <Image source={uri: {'https://facebook.github.io/react/img/logo_og.png'}} />}
-            renderSelectedIcon={() => <Image source={uri: {'https://facebook.github.io/react/img/logo_og.png'}} />}
-            badgeText="1"
-            onPress={() => this.setState({ selectedTab: 'home' })}>
-            {homeView}
-          </TabNavigator.Item>
-          <TabNavigator.Item
-            selected={this.state.selectedTab === 'profile'}
-            title="Profile"
-            renderIcon={() => <Image source={uri: {'https://facebook.github.io/react/img/logo_og.png'}} />}
-            renderSelectedIcon={() => <Image source={uri: {'https://facebook.github.io/react/img/logo_og.png'}} />}
-            renderBadge={() => <CustomBadgeView />}
-            onPress={() => this.setState({ selectedTab: 'profile' })}>
-            {profileView}
-          </TabNavigator.Item>
-        </TabNavigator>
-      </View>
+      <TabNavigator>
+        <TabNavigator.Item
+          selected={this.state.selectedTab === 'home'}
+          title="Home"
+          renderIcon={() => <Image source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} />}
+          renderSelectedIcon={() => <Image source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} />}
+          badgeText="1"
+          onPress={() => this.setState({ selectedTab: 'home' })}>
+          {homeView}
+        </TabNavigator.Item>
+        <TabNavigator.Item
+          selected={this.state.selectedTab === 'profile'}
+          title="Profile"
+          renderIcon={() => <Image source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} />}
+          renderSelectedIcon={() => <Image source={{uri: 'http://facebook.github.io/react/img/logo_og.png'}} />}
+          renderBadge={() => <CustomBadgeView />}
+          onPress={() => this.setState({ selectedTab: 'profile' })}>
+          {profileView}
+        </TabNavigator.Item>
+      </TabNavigator>
     );
   }
 }
